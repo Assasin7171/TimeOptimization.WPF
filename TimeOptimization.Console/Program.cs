23f559c96d5c0ad8a45ebcff62636e5a09624852
KonradPC <KonradPC@KONRAD_PC>
2024-06-17 21:53:53 +0200

    Dodaj pliki projektów.

---
+++ TimeOptimization.Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TimeOptimization.Model.Models;
+
+namespace TimeOptimizationConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AppCoreModel model = new AppCoreModel();
+            model.GetProcesses();
+        }
+    }
+}
---
+++ TimeOptimization.Model/Models/AppCoreModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeOptimization.Model.Models
+{
+    public class AppCoreModel
+    {
+        public void GetProcesses()
+        {
+            Process[] processes = Process.GetProcesses();
+
+            foreach (Process process in processes)
+            {
+                try
+                {
+                    Console.WriteLine($"Nazwa: {process.ProcessName}, ID: {process.Id}");
+                }
+                catch( Exception ex )
+                {
+                    Console.WriteLine($"Nie można uzyskać informacji o procesie: {ex.Message}");
+                }
+            }
+        }
+    }
+}
